4a1db03555efd9da223a9af90fd0988b921130e1
Zion Mateo <mateoz@uci.edu>
2025-06-06 22:00:17 -0700

    gh page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stellar-evo-sim', // Replace with your actual repo name
+  base: '/p20b', // Replace with your actual repo name
 })
